352108b936aab213954be3a850bb48e777986cd8
Abhinav Singh <abhinav.s1997@yahoo.com>
2018-11-06 19:13:22 -0800

    Add files via upload

---
+++ CARS-info.sql
@@ -0,0 +1,46 @@
+-- Abhinav Singh and Jake Veazey
+-- asingh54@calpoly.edu and  
+
+-- Reports the name and YearMade of each renault
+SELECT c.Description, d.YearMade
+FROM carNames c, carsData d
+WHERE c.Model = 'renault' AND c.Id = d.Id 
+ORDER BY d.YearMade
+;
+
+-- All volvo cars produced between 1977 and 1981, names reported along with the YearMade
+SELECT c.Description, d.YearMade 
+FROM carNames c, carsData d
+WHERE c.Model = 'volvo' AND d.YearMade >= 1977 AND d.YearMade <= 1981 AND c.Id = d.Id
+ORDER BY d.YearMade
+;
+
+-- All asian automakers with names and country of origin
+SELECT m.FullName, c.Name
+FROM carMakers m, countries c, continents cc
+WHERE cc.Name = 'Asia' AND c.Continent = cc.Id AND c.Id = m.Country
+ORDER BY m.FullName
+;
+
+-- All four cyclinder cars produced in 1980 with fuel economies of 20 MPG and that accelerate to 60MPH
+-- in unrder 15 seconds 
+
+SELECT c.Description, m.FullName
+FROM carNames c, carMakers m, carsData d, modelList ml
+WHERE d.Cylinders != 4 AND d.MPG > 20 AND d.Accelerate < 15 AND d.YearMade = 1980
+      AND d.Id = c.Id AND c.Model = ml.Model AND ml.Maker = m.Id
+ORDER BY d.Cylinders, d.YearMade, d.MPG, d.Accelerate
+;
+
+SELECT DISTINCT m.FullName, c.Name
+FROM carsData cd, carNames cn, modelList ml, carMakers m, countries c, continents cc
+WHERE c.Id = cn.Id AND cn.Model = ml.Model AND ml.Maker = m.Id AND m.Country = c.Id
+      AND c.Continent = cc.Id AND cc.Name != 'europe' AND cd.YearMade >= 1979 AND cd.YearMade <= 1981
+      AND cd.Weight < 2000
+;
+
+SELECT c.Description, d.YearMade, d.Weight / d.Horsepower AS Ratio
+FROM carsData d, carNames c
+WHERE c.Model = 'saab' AND d.YearMade > 1978 AND c.Id = d.Id 
+ORDER BY Ratio DESC
+;
---
+++ STUDENTS-info.sql
@@ -0,0 +1,38 @@
+-- Abhinav Singh and Jake Veazey
+-- asingh54@calpoly.edu 
+
+
+--This should output all students first and last names, sorted by last name for
+-- anyone in classroom 111
+
+SELECT FirstName, LastName
+FROM list
+WHERE Classroom = 111
+ORDER BY LastName
+;
+
+-- Orders classroom and outputs the classroom number + grade
+
+SELECT DISTINCT Classroom, Grade 
+FROM list
+ORDER BY Classroom DESC
+;
+
+-- Reports the first and last name along with the room number for all teachers who teach fifth grade
+SELECT DISTINCT t.FirstName, t.LastName, t.Classroom
+FROM teachers t, list s 
+WHERE s.Grade = 5 AND s.Classroom = t.Classroom
+ORDER BY t.Classroom
+;
+
+-- Finds all students who are taught by Otha Moyer and outputs those students 
+SELECT s.FirstName, s.LastName
+FROM teachers t, list s
+WHERE t.FirstName = 'OTHA' AND t.LastName = 'MOYER' AND s.Classroom = t.Classroom
+ORDER BY s.LastName
+;
+-- Gets all teachers who teacher K through 3rd grade 
+SELECT DISTINCT t.FirstName, t.LastName, s.Grade
+FROM teachers t, list s
+WHERE s.Classroom = t.Classroom AND s.Grade >= 0 AND s.Grade <=3
+ORDER BY t.LastName, s.Grade
